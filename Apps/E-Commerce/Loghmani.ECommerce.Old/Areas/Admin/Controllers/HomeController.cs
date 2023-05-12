4ec8a36980daf95d3418b1a23cdb3ec795b1dc8b
ahadvirus <ahadvirus@yahoo.com>
2023-05-12 14:48:34 +0330

    Create admin area for dashboard

---
+++ Apps/E-Commerce/Loghmani.ECommerce.Old/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Loghmani.ECommerce.Old.Infrastructures.Configurations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loghmani.ECommerce.Old.Areas.Admin.Controllers;
+
+[Area(nameof(Area.Admin))]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
