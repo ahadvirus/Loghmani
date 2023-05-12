897b1fbebf6920aeb231c0b9df84dfc8ce481947
ahadvirus <ahadvirus@yahoo.com>
2023-05-12 14:49:43 +0330

    Add area map route

--- Apps/E-Commerce/Loghmani.ECommerce.Old/Startup.cs
+++ Apps/E-Commerce/Loghmani.ECommerce.Old/Startup.cs
@@ -27,6 +27,14 @@ public static class Startup
 
         app.UseAuthorization();
 
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+        });
+
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
