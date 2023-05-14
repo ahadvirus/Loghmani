b93d9a6b234cdcb73e54acc91febc347e3c6f49c
ahadvirus <ahadvirus@yahoo.com>
2023-05-14 09:14:24 +0330

    Create some extensions

--- Apps/E-Commerce/Loghmani.ECommerce.Old/Infrastructures/Extensions/HttpContextExtension.cs
+++ Apps/E-Commerce/Loghmani.ECommerce.Old/Infrastructures/Extensions/HttpContextExtension.cs
@@ -1,5 +1,11 @@
-﻿namespace Loghmani.ECommerce.Old.Infrastructures.Extensions;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Loghmani.ECommerce.Old.Infrastructures.Extensions;
 
 public static class HttpContextExtension
 {
+    public static string GetHostName(this HttpContext context)
+    {
+        return string.Format("{0}://{1}", context.Request.Scheme, context.Request.Host);
+    }
 }
--- Apps/E-Commerce/Loghmani.ECommerce.Old/Infrastructures/Extensions/StringExtension.cs
+++ Apps/E-Commerce/Loghmani.ECommerce.Old/Infrastructures/Extensions/StringExtension.cs
@@ -1,5 +1,14 @@
-﻿namespace Loghmani.ECommerce.Old.Infrastructures.Extensions;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Loghmani.ECommerce.Old.Infrastructures.Extensions;
 
 public static class StringExtension
 {
+    public static string RemoveController(this string value)
+    {
+        return value.Replace(
+            nameof(Controller),
+            string.Empty
+        );
+    }
 }
