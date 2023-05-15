953c94277a07bfc133209300f2c6095b171678db
ahadvirus <ahadvirus@yahoo.com>
2023-05-15 14:03:30 +0330

    New configuration for data annotations

---
+++ Apps/E-Commerce/Loghmani.ECommerce.Old/Infrastructures/Configurations/DataAnnotations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Loghmani.ECommerce.Old.Infrastructures.Configurations
+{
+    public class DataAnnotations
+    {
+        public string Required
+        {
+            get
+            {
+                return nameof(RequiredAttribute).Replace(oldValue: nameof(Attribute), newValue: string.Empty);
+            }
+        }
+    }
+}
